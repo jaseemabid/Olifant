e5f8328a274dcccf5919edf0f87cb191fdcfa9ff
Jaseem Abid <jaseemabid@gmail.com>
2017-05-30 15:43:32 -0400

    Clean up the code a bit

--- src/Olifant.hs
+++ src/Olifant.hs
@@ -3,7 +3,6 @@
 
 module Olifant where
 
-import           Control.Applicative
 import           Control.Monad.Except
 import           Control.Monad.State
 import           LLVM.AST
@@ -14,124 +13,125 @@ import           LLVM.Context
 import           LLVM.Module
 
 ---
---- AST
+--- Language definition
 ---
 
--- Simple integer language with just numbers, + and -
+-- | Simple integer language with just numbers, + and -
 data Calc = Number Integer
           | Plus Calc Calc
 
----
---- LLVM Types
----
-
--- The only supported type as of now
+-- | 64 bit integer - the only supported type as of now
 number :: Type
 number = IntegerType 64
 
 ---
---- Codegen
+--- State
 ---
 
--- type SymbolTable = [(String, Operand)]
--- type Names = Map.Map String Int
+-- | Stack of instructions
+newtype CodegenState = CodegenState {
+    stack :: [Named Instruction]
+  } deriving Show
 
-
--- %foo = add 1 2
-newtype CodegenState = CodegenState [Named Instruction] -- Stack of instructions
-    deriving Show
-
+-- | Codegen state monad
 newtype Codegen a = Codegen { runCodegen :: State CodegenState a }
     deriving (Functor, Applicative, Monad, MonadState CodegenState)
 
--- Specialized state monad holding a module
+-- | Specialized state monad holding a module
 newtype LLVM a = LLVM (State AST.Module a)
   deriving (Functor, Applicative, Monad, MonadState AST.Module)
 
--- Runs the operation with the specific state
+-- | Runs the operation with an initial state
 runLLVM :: AST.Module -> LLVM a -> AST.Module
 runLLVM modl (LLVM m) = execState m modl
 
--- Add source file as well for AOT
-emptyModule :: String -> AST.Module
-emptyModule label = defaultModule { moduleName = label }
+---
+--- State manipulators
+---
 
+-- | Add a definition to the module
 addDefn :: Definition -> LLVM ()
 addDefn d = do
   defs <- gets moduleDefinitions
   modify $ \s -> s { moduleDefinitions = defs ++ [d] }
 
--- define :: --
--- external :: --
+-- Count
+count :: Codegen Int
+count = length <$> gets stack
 
---  1 + 3
+---
+--- Manipulate LLVM types
+---
 
--- Add stuff into main block
+-- | Make a fresh unnamed variable; %4 or %5
+unnamed :: Codegen Name
+unnamed = UnName . fromIntegral <$> count
+
+-- | References
+local ::  Name -> Operand
+local = LocalReference number
+
+-- | Create a module from name
+--
+-- Note: Add source file as well for AOT compilation
+mkModule :: String -> AST.Module
+mkModule label = defaultModule { moduleName = label }
+
+-- | Create a simple block with fixed terminator
 mkBasicBlock :: Codegen BasicBlock
 mkBasicBlock = do
-    ins <- stack
+    instructions <- gets stack
     term <- terminator
-    new <- fresh
-    return $ BasicBlock (Name "entry") ins (new := term)
+    new <- unnamed
+    return $ BasicBlock (Name "entry") instructions (new := term)
 
-count :: Codegen Int
-count = do
-  CodegenState st <- get
-  return $ length st
-
-fresh :: Codegen Name
-fresh = UnName . fromIntegral <$> count
-
-stack :: Codegen [Named Instruction]
-stack = do
-  CodegenState st <- get
-  return st
-
--- | Add an instruction to state; name
+-- | Name an instruction and add to stack
 --
 -- Converts a `Add 1 2` to `%6 = Add 1 2`
 instr :: Instruction -> Codegen Operand
 instr ins = do
   n <- count
-  let ref = Name $ show $ fromIntegral n
-
+  -- XXX: This is potentially wrong
+  let ref = Name $ show n
   let bound = ref := ins
+  -- XXX: Abstract this away
   modify $ \(CodegenState s) -> CodegenState $ s ++ [bound]
   return $ local ref
 
--- a + b ; operand for this
-
--- References
-local ::  Name -> Operand
-local = LocalReference number
-
+-- | Step through the AST
+--
+-- This is wrong, needs a rewrite
 step :: Calc -> Codegen Operand
 step (Number n) = return $ ConstantOperand $ Int 64 n
 step (Plus a b) = do
-    -- l <- fresh
+    -- l <- unnamed
     lhs <- step a
     -- let ls = l := lhs
 
-    -- r <- fresh
+    -- r <- unnamed
     rhs <- step b
 
     -- let rs = r := rhs
 
     instr $ LLVM.AST.FAdd NoFastMathFlags lhs rhs []
 
+-- | Return the last expression from a block
+--
+-- This is wrong. Should return strictly an unnamed variable
 terminator :: Codegen Terminator
 terminator = do
   n <- count
   -- %5 -> return %5
-  let ret ref = Ret $ Just $ local $ Name $ show $ fromIntegral $ ref-1
+  let ret ref = Ret $ Just $ local $ Name $ show $ ref - 1
   return $ ret n []
 
-dummy ::  Codegen ()
-dummy = return ()
+---
+--- Everything below this line is crap
+---
 
 run :: Calc -> LLVM AST.Module
 run calc = do
-    mod <- get
+    module' <- get
     let x = execState (runCodegen (step calc)) (CodegenState []) :: CodegenState
     let block = evalState (runCodegen mkBasicBlock) x
 
@@ -139,7 +139,7 @@ run calc = do
     let main'' = GlobalDefinition $ main' [block]
 
     addDefn main''
-    return mod
+    return module'
 
   where
       main' :: [BasicBlock] -> Global
@@ -149,9 +149,6 @@ run calc = do
           , basicBlocks = blocks
         }
 
-zero :: Calc
-zero = Number 0
-
 seven :: Calc
 seven = Plus (Number 4)  (Plus (Number 1) (Number 2))
 
@@ -160,13 +157,10 @@ liftError = runExceptT >=> either fail return
 
 main :: IO ()
 main = do
-    let modn = (emptyModule "calc") :: AST.Module
+    let modn = mkModule "calc" :: AST.Module
 
     withContext $ \context -> do
         let newast = runLLVM modn (run seven)
         liftError $ withModuleFromAST context newast $ \m -> do
         llstr <- moduleLLVMAssembly m
         putStrLn llstr
-
-    -- print $ moduleLLVMAssembly $
-    -- return ()
