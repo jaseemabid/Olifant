851a1289220f98d946a687d27ee4c0115eb9286d
Jaseem Abid <jaseemabid@gmail.com>
2017-06-03 21:09:01 -0400

    Addd rudimenary support for variables

--- src/Olifant.hs
+++ src/Olifant.hs
@@ -4,10 +4,13 @@
 module Olifant where
 
 import Prelude hiding (mod)
+
 import Control.Monad.Except
 import Control.Monad.State
+import Data.Default
 import Data.Text.Lazy.IO as TIO
 import LLVM.AST
+import LLVM.AST.AddrSpace (AddrSpace(..))
 import LLVM.AST.Constant (Constant(..))
 import LLVM.AST.Global (Global(..))
 import LLVM.Context (withContext)
@@ -21,20 +24,38 @@ import LLVM.Pretty (ppllvm)
 -- | Simple integer language with just numbers, + and -
 data Calc = Number Integer
           | Plus Calc Calc
+          | Binding String
+          | Assignment String Calc
 
--- | 64 bit integer - the only supported type as of now
+-- | 64 bit integer
 number :: Type
 number = IntegerType 64
+
+-- | Pointer to the `number` type
+pointer :: Type
+pointer = PointerType number $ AddrSpace 0
 
 ---
 --- State
 ---
 
+-- | Symbol tables maps a string to a LLVM operand
+type SymbolTable = [(String, Operand)]
+
 -- | Stack of instructions
-data CodegenState = CodegenState {
-    stack :: [Named Instruction]
-  , counter :: Int
-  } deriving Show
+data CodegenState =
+    CodegenState
+    { stack  :: [Named Instruction]  -- List of operations
+    , symtab :: SymbolTable -- Symbol table
+    , counter :: Int
+    } deriving Show
+
+instance Default CodegenState where
+    def = CodegenState
+      { stack = []
+      , symtab = []
+      , counter = 0
+      }
 
 -- | Codegen state monad
 newtype Codegen a = Codegen {
@@ -52,94 +73,151 @@ runLLVM :: Module -> LLVM a -> Module
 runLLVM modl (LLVM m) = execState m modl
 
 ---
---- Codegen handlers
+--- Primitive wrappers
 ---
 
--- | Make a fresh unnamed variable; %4 or %5
-unnamed :: Codegen Name
-unnamed =  do
-    n <- gets counter
-    modify $ \s -> s {counter = n + 1}
-    return $ UnName . fromIntegral $ n
+-- | Variable assignment
+--
+-- Converts expression of the form `%1 = 2` to `* %1 = 2`
+--
+-- XXX: Having to use `error` here is shitty
+store :: Operand -> Operand -> Codegen ()
+store (LocalReference _type n) val =
+    push $ Do $ Store False ptr val Nothing 0 []
+  where
+    ptr = LocalReference pointer n
+store _ _ = error "Cannot store anything other than local reference"
+
+-- | Fetch a variable from memory
+--
+-- Enforce operand is a pointer type. This is shitty types :/
+load :: Operand -> Codegen Operand
+load (LocalReference _type n) = unnamed $ Load False ptr Nothing 0 []
+  where
+    ptr = LocalReference pointer n
+load _ = error "Cannot load anything other than local pointer reference"
+
+alloca :: Type -> Maybe String -> Codegen Operand
+alloca ty Nothing = unnamed $ Alloca ty Nothing 0 []
+alloca ty (Just ref)= named ref $ Alloca ty Nothing 0 []
+
+assign :: String -> Operand -> Codegen ()
+assign var x = modify $ \s -> s { symtab = (var, x) : symtab s }
+
+add :: Operand -> Operand -> Instruction
+add lhs rhs = LLVM.AST.Add False False lhs rhs []
+
+---
+--- AST Manipulation
+---
+
+-- | Name an instruction and add to stack.
+--
+--  - Takes an expression of the form `Add 1 2`
+--  - Gets a fresh name for it, `%2`
+--  - Adds `%2 = Add 1 2` to the stack
+--  - Returns `%2`
+--
+unnamed :: Instruction -> Codegen Operand
+unnamed ins = do
+    new <- fresh
+    push $ new := ins
+    return $ LocalReference number new
+  where
+    -- | Make a fresh unnamed variable; %4 or %5
+    fresh :: Codegen Name
+    fresh =  do
+        n <- gets counter
+        modify $ \s -> s {counter = n + 1}
+        return $ UnName . fromIntegral $ n
+
+-- | Add the instruction to the stack with a specific name.
+--
+--  - Takes an expression of the form `Add 1 2` and a name `%foo`
+--  - Adds `%foo = Add 1 2` to the stack
+--  - Returns `%foo`
+--
+named :: String -> Instruction -> Codegen Operand
+named str ins =
+    push (op := ins) >> return (LocalReference number op)
+  where
+    op :: Name
+    op = Name str
+
+-- | Push a named instruction to stack
+push :: Named Instruction -> Codegen ()
+push ins = modify $ \s -> s {stack = stack s ++ [ins]}
 
 -- | Step through the AST
 --
--- This is wrong, needs a rewrite
+-- Step should return an operand, which is the LHS of the operand it just dealt
+-- with. Step is free to push instructions to the current block.
 step :: Calc -> Codegen Operand
 
+-- | Make a constant operand out of the constant and return it.
 step (Number n) = return $ ConstantOperand $ Int 64 n
 
+-- | Get the reference to the operands on LHS and RH'S. Push to stack the
+-- instruction to compute the new value and return the operand that refers to
+-- the new value.
 step (Plus a b) = do
     lhs <- step a :: Codegen Operand
     rhs <- step b :: Codegen Operand
+    unnamed $ add lhs rhs
 
-    -- %result = fadd 1 2a
-    instr $ LLVM.AST.Add False False lhs rhs []
+step (Assignment bind ref) = do
+    result <- step ref
+    ptr <- alloca number (Just bind) :: Codegen Operand
+    store ptr result
+    assign bind ptr
+    return ptr
 
--- | References
-local ::  Name -> Operand
-local = LocalReference number
+-- | Find the operand for the variable from the symbol table and return it.
+step (Binding binding) = do
+    symbols <- gets symtab
+    case lookup binding symbols of
+        Just op -> return op
+        Nothing -> error $ "> Undefined variable " ++ binding
+
+---
+--- Module level elements
+---
+
+-- | Create a simple block from a list of instructions and a terminator
+mkBasicBlock :: [Named Instruction] -> Named Terminator -> BasicBlock
+mkBasicBlock = BasicBlock (Name "entry")
 
 -- | Return the last expression from a block
---
--- This is wrong. Should return strictly an unnamed variable
-terminator :: Codegen (Named Terminator)
-terminator = do
-  n <- gets counter
-  return $ Do $ Ret (Just $ local $ UnName $ fromIntegral n - 1) []
+mkTerminator :: Operand -> Codegen (Named Terminator)
+mkTerminator result = do
+    r <- load result
+    return $ Do $ Ret (Just r) []
 
 ---
---- LLVM type handlers
+--- Default values
 ---
 
--- | Create a module from name
---
--- Note: Add source file as well for AOT compilation
-mkModule :: String -> Module
-mkModule label = defaultModule { moduleName = label }
-
--- | Create a simple block with fixed terminator
-mkBasicBlock :: Codegen BasicBlock
-mkBasicBlock = do
-    instructions <- gets stack
-    term <- terminator
-    return $ BasicBlock (Name "entry") instructions term
-
--- | Name an instruction and add to stack
---
--- Converts a `Add 1 2` to `%6 = Add 1 2`
-instr :: Instruction -> Codegen Operand
-instr ins = do
-  new <- unnamed
-  let bound = new := ins
-  modify $ \s -> s {stack = stack s ++ [bound]}
-  return $ LocalReference number new
+progn :: Calc
+progn = Assignment "a" $ Plus (Number 1) (Plus (Number 2) (Number 3))
 
 ---
---- LLVM handlers
+--- Code generation
 ---
 
--- | Add a definition to the module
-addDefn :: Definition -> LLVM ()
-addDefn d = do
-  definitions <- gets moduleDefinitions
-  modify $ \s -> s { moduleDefinitions = definitions ++ [d] }
-
----
---- Everything below this line is crap
----
-
-run :: Calc -> LLVM Module
+run :: Calc -> Codegen Module
 run calc = do
-    module' <- get
-    let instructions = execState (runCodegen (step calc)) codegen
-    let block = evalState (runCodegen mkBasicBlock) instructions
+    result <- step calc
+    terminator <- mkTerminator result
 
-    -- add block to main
+    ins <- gets stack
+
+    let block = mkBasicBlock ins terminator
     let main'' = GlobalDefinition $ main' [block]
 
-    addDefn main''
-    return module'
+    return $ defaultModule {
+          moduleName = "calc"
+        , moduleDefinitions = [main'']
+        }
 
   where
       main' :: [BasicBlock] -> Global
@@ -149,37 +227,21 @@ run calc = do
           , basicBlocks = blocks
         }
 
-      -- | Default CodegenState
-      codegen :: CodegenState
-      codegen = CodegenState [] 0
-
----
---- A simple sample program
----
-
-progn :: Calc
-progn = Plus (Number 1)  (Plus (Number 2) (Number 3))
-
----
---- Top level data
----
-
--- | Module to encapsulate everything
-modn :: Module
-modn = mkModule "calc"
-
 -- | Generate native code with C++ FFI
 toLLVM :: Module -> IO ()
-toLLVM modl = withContext $ \context -> do
-    errOrLLVM <- runExceptT $ withModuleFromAST context modl moduleLLVMAssembly
+toLLVM mod = withContext $ \context -> do
+    errOrLLVM <- runExceptT $ withModuleFromAST context mod moduleLLVMAssembly
     case errOrLLVM of
       Left err -> Prelude.putStrLn $ "error: " ++ err
       Right llvm -> Prelude.putStrLn llvm
 
+compile :: Calc -> Module
+compile prog = evalState (runCodegen (run prog)) def
+
 -- | Generate native code with C++ FFI
 pretty :: Calc -> IO ()
-pretty prog = TIO.putStrLn $ ppllvm $ runLLVM modn (run prog)
+pretty = TIO.putStrLn . ppllvm . compile
 
 -- | Print compiled LLVM IR to stdout
 main :: IO ()
-main = toLLVM $ runLLVM modn (run progn)
+main = toLLVM $ compile progn
