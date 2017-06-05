62d4000bf87aea182ed1d8fea2c3161ec6ddf8c2
Jaseem Abid <jaseemabid@gmail.com>
2017-06-05 13:46:52 -0400

    Cleanup with hindent

--- src/Olifant.hs
+++ src/Olifant.hs
@@ -10,9 +10,9 @@ import Control.Monad.State
 import Data.Default
 import Data.Text.Lazy.IO as TIO
 import LLVM.AST
-import LLVM.AST.AddrSpace (AddrSpace(..))
-import LLVM.AST.Constant (Constant(..))
-import LLVM.AST.Global (Global(..))
+import LLVM.AST.AddrSpace
+import LLVM.AST.Constant
+import LLVM.AST.Global
 import LLVM.Context (withContext)
 import LLVM.Module (moduleLLVMAssembly, withModuleFromAST)
 import LLVM.Pretty (ppllvm)
@@ -20,12 +20,14 @@ import LLVM.Pretty (ppllvm)
 ---
 --- Language definition
 ---
-
 -- | Simple integer language with just numbers, + and -
-data Calc = Number Integer
-          | Plus Calc Calc
-          | Binding String
-          | Assignment String Calc
+data Calc
+    = Number Integer
+    | Plus Calc
+           Calc
+    | Binding String
+    | Assignment String
+                 Calc
 
 -- | 64 bit integer
 number :: Type
@@ -38,35 +40,30 @@ pointer = PointerType number $ AddrSpace 0
 ---
 --- State
 ---
-
 -- | Symbol tables maps a string to a LLVM operand
 type SymbolTable = [(String, Operand)]
 
 -- | Stack of instructions
-data CodegenState =
-    CodegenState
-    { stack  :: [Named Instruction]  -- List of operations
+data CodegenState = CodegenState
+    { stack :: [Named Instruction] -- List of operations
     , symtab :: SymbolTable -- Symbol table
     , counter :: Int
-    } deriving Show
+    } deriving (Show)
 
 instance Default CodegenState where
-    def = CodegenState
-      { stack = []
-      , symtab = []
-      , counter = 0
-      }
+    def = CodegenState {stack = [], symtab = [], counter = 0}
 
 -- | Codegen state monad
-newtype Codegen a = Codegen {
-    runCodegen :: State CodegenState a
-  } deriving (Functor, Applicative, Monad, MonadState CodegenState)
+newtype Codegen a = Codegen
+    { runCodegen :: State CodegenState a
+    } deriving (Functor, Applicative, Monad, MonadState CodegenState)
 
 -- | A specialized state monad holding a Module
 --
 -- The state monad upon evaluation will emit a Module containing the AST.
-newtype LLVM a = LLVM (State Module a)
-  deriving (Functor, Applicative, Monad, MonadState Module)
+newtype LLVM a =
+    LLVM (State Module a)
+    deriving (Functor, Applicative, Monad, MonadState Module)
 
 -- | Runs the operation with an initial state
 runLLVM :: Module -> LLVM a -> Module
@@ -75,7 +72,6 @@ runLLVM modl (LLVM m) = execState m modl
 ---
 --- Primitive wrappers
 ---
-
 -- | Variable assignment
 --
 -- Converts expression of the form `%1 = 2` to `* %1 = 2`
@@ -99,10 +95,10 @@ load _ = error "Cannot load anything other than local pointer reference"
 
 alloca :: Type -> Maybe String -> Codegen Operand
 alloca ty Nothing = unnamed $ Alloca ty Nothing 0 []
-alloca ty (Just ref)= named ref $ Alloca ty Nothing 0 []
+alloca ty (Just ref) = named ref $ Alloca ty Nothing 0 []
 
 assign :: String -> Operand -> Codegen ()
-assign var x = modify $ \s -> s { symtab = (var, x) : symtab s }
+assign var x = modify $ \s -> s {symtab = (var, x) : symtab s}
 
 add :: Operand -> Operand -> Instruction
 add lhs rhs = LLVM.AST.Add False False lhs rhs []
@@ -110,7 +106,6 @@ add lhs rhs = LLVM.AST.Add False False lhs rhs []
 ---
 --- AST Manipulation
 ---
-
 -- | Name an instruction and add to stack.
 --
 --  - Takes an expression of the form `Add 1 2`
@@ -123,10 +118,10 @@ unnamed ins = do
     new <- fresh
     push $ new := ins
     return $ LocalReference number new
-  where
     -- | Make a fresh unnamed variable; %4 or %5
+  where
     fresh :: Codegen Name
-    fresh =  do
+    fresh = do
         n <- gets counter
         modify $ \s -> s {counter = n + 1}
         return $ UnName . fromIntegral $ n
@@ -138,8 +133,7 @@ unnamed ins = do
 --  - Returns `%foo`
 --
 named :: String -> Instruction -> Codegen Operand
-named str ins =
-    push (op := ins) >> return (LocalReference number op)
+named str ins = push (op := ins) >> return (LocalReference number op)
   where
     op :: Name
     op = Name str
@@ -153,10 +147,8 @@ push ins = modify $ \s -> s {stack = stack s ++ [ins]}
 -- Step should return an operand, which is the LHS of the operand it just dealt
 -- with. Step is free to push instructions to the current block.
 step :: Calc -> Codegen Operand
-
 -- | Make a constant operand out of the constant and return it.
 step (Number n) = return $ ConstantOperand $ Int 64 n
-
 -- | Get the reference to the operands on LHS and RH'S. Push to stack the
 -- instruction to compute the new value and return the operand that refers to
 -- the new value.
@@ -164,14 +156,12 @@ step (Plus a b) = do
     lhs <- step a :: Codegen Operand
     rhs <- step b :: Codegen Operand
     unnamed $ add lhs rhs
-
 step (Assignment bind ref) = do
     result <- step ref
     ptr <- alloca number (Just bind) :: Codegen Operand
     store ptr result
     assign bind ptr
     return ptr
-
 -- | Find the operand for the variable from the symbol table and return it.
 step (Binding binding) = do
     symbols <- gets symtab
@@ -182,7 +172,6 @@ step (Binding binding) = do
 ---
 --- Module level elements
 ---
-
 -- | Create a simple block from a list of instructions and a terminator
 mkBasicBlock :: [Named Instruction] -> Named Terminator -> BasicBlock
 mkBasicBlock = BasicBlock (Name "entry")
@@ -196,37 +185,29 @@ mkTerminator result = do
 ---
 --- Code generation
 ---
-
 run :: Calc -> Codegen Module
 run calc = do
     result <- step calc
     terminator <- mkTerminator result
-
     ins <- gets stack
-
     let block = mkBasicBlock ins terminator
     let main'' = GlobalDefinition $ main' [block]
-
-    return $ defaultModule {
-          moduleName = "calc"
-        , moduleDefinitions = [main'']
-        }
-
+    return $ defaultModule {moduleName = "calc", moduleDefinitions = [main'']}
   where
-      main' :: [BasicBlock] -> Global
-      main' blocks = functionDefaults {
-          name = Name "main"
-          , returnType = number
-          , basicBlocks = blocks
-        }
+    main' :: [BasicBlock] -> Global
+    main' blocks =
+        functionDefaults
+        {name = Name "main", returnType = number, basicBlocks = blocks}
 
 -- | Generate native code with C++ FFI
 toLLVM :: Module -> IO ()
-toLLVM mod = withContext $ \context -> do
-    errOrLLVM <- runExceptT $ withModuleFromAST context mod moduleLLVMAssembly
-    case errOrLLVM of
-      Left err -> Prelude.putStrLn $ "error: " ++ err
-      Right llvm -> Prelude.putStrLn llvm
+toLLVM mod =
+    withContext $ \context -> do
+        errOrLLVM <-
+            runExceptT $ withModuleFromAST context mod moduleLLVMAssembly
+        case errOrLLVM of
+            Left err -> Prelude.putStrLn $ "error: " ++ err
+            Right llvm -> Prelude.putStrLn llvm
 
 compile :: Calc -> Module
 compile prog = evalState (runCodegen (run prog)) def
--- test/Test.hs
+++ test/Test.hs
@@ -34,100 +34,109 @@ progn = Assignment "a" $ Plus (Number 1) (Plus (Number 2) (Number 3))
 -- | Module for `a = 1 + (2 + 3)`
 m :: Module
 m =
-  Module
-  { moduleName = "calc"
-  , moduleSourceFileName = "<string>"
-  , moduleDataLayout = Nothing
-  , moduleTargetTriple = Nothing
-  , moduleDefinitions =
-      [ GlobalDefinition
-          Function
-          { linkage = External
-          , visibility = Default
-          , dllStorageClass = Nothing
-          , callingConvention = C
-          , returnAttributes = []
-          , returnType = IntegerType {typeBits = 64}
-          , name = Name "main"
-          , parameters = ([], False)
-          , functionAttributes = []
-          , section = Nothing
-          , comdat = Nothing
-          , alignment = 0
-          , garbageCollectorName = Nothing
-          , prefix = Nothing
-          , basicBlocks =
-              [ BasicBlock
-                  (Name "entry")
-                  [ UnName 0 :=
-                    LLVM.AST.Instruction.Add
-                    { nsw = False
-                    , nuw = False
-                    , operand0 =
-                        ConstantOperand Int {integerBits = 64, integerValue = 2}
-                    , operand1 =
-                        ConstantOperand Int {integerBits = 64, integerValue = 3}
-                    , metadata = []
-                    }
-                  , UnName 1 :=
-                    LLVM.AST.Instruction.Add
-                    { nsw = False
-                    , nuw = False
-                    , operand0 =
-                        ConstantOperand Int {integerBits = 64, integerValue = 1}
-                    , operand1 =
-                        LocalReference IntegerType {typeBits = 64} (UnName 0)
-                    , metadata = []
-                    }
-                  , Name "a" :=
-                    Alloca
-                    { allocatedType = IntegerType {typeBits = 64}
-                    , numElements = Nothing
-                    , alignment = 0
-                    , metadata = []
-                    }
-                  , Do
-                      Store
-                      { volatile = False
-                      , address =
-                          LocalReference
-                            PointerType
-                            { pointerReferent = IntegerType {typeBits = 64}
-                            , pointerAddrSpace = AddrSpace 0
-                            }
-                            (Name "a")
-                      , value =
-                          LocalReference IntegerType {typeBits = 64} (UnName 1)
-                      , maybeAtomicity = Nothing
-                      , alignment = 0
-                      , metadata = []
-                      }
-                  , UnName 2 :=
-                    Load
-                    { volatile = False
-                    , address =
-                        LocalReference
-                          PointerType
-                          { pointerReferent = IntegerType {typeBits = 64}
-                          , pointerAddrSpace = AddrSpace 0
-                          }
-                          (Name "a")
-                    , maybeAtomicity = Nothing
-                    , alignment = 0
-                    , metadata = []
-                    }
-                  ]
-                  (Do
-                     Ret
-                     { returnOperand =
-                         Just
-                           (LocalReference
-                              IntegerType {typeBits = 64}
-                              (UnName 2))
-                     , metadata' = []
-                     })
-              ]
-          , personalityFunction = Nothing
-          }
-      ]
-  }
+    Module
+    { moduleName = "calc"
+    , moduleSourceFileName = "<string>"
+    , moduleDataLayout = Nothing
+    , moduleTargetTriple = Nothing
+    , moduleDefinitions =
+          [ GlobalDefinition
+                Function
+                { linkage = External
+                , visibility = Default
+                , dllStorageClass = Nothing
+                , callingConvention = C
+                , returnAttributes = []
+                , returnType = IntegerType {typeBits = 64}
+                , name = Name "main"
+                , parameters = ([], False)
+                , functionAttributes = []
+                , section = Nothing
+                , comdat = Nothing
+                , alignment = 0
+                , garbageCollectorName = Nothing
+                , prefix = Nothing
+                , basicBlocks =
+                      [ BasicBlock
+                            (Name "entry")
+                            [ UnName 0 :=
+                              LLVM.AST.Instruction.Add
+                              { nsw = False
+                              , nuw = False
+                              , operand0 =
+                                    ConstantOperand
+                                        Int {integerBits = 64, integerValue = 2}
+                              , operand1 =
+                                    ConstantOperand
+                                        Int {integerBits = 64, integerValue = 3}
+                              , metadata = []
+                              }
+                            , UnName 1 :=
+                              LLVM.AST.Instruction.Add
+                              { nsw = False
+                              , nuw = False
+                              , operand0 =
+                                    ConstantOperand
+                                        Int {integerBits = 64, integerValue = 1}
+                              , operand1 =
+                                    LocalReference
+                                        IntegerType {typeBits = 64}
+                                        (UnName 0)
+                              , metadata = []
+                              }
+                            , Name "a" :=
+                              Alloca
+                              { allocatedType = IntegerType {typeBits = 64}
+                              , numElements = Nothing
+                              , alignment = 0
+                              , metadata = []
+                              }
+                            , Do
+                                  Store
+                                  { volatile = False
+                                  , address =
+                                        LocalReference
+                                            PointerType
+                                            { pointerReferent =
+                                                  IntegerType {typeBits = 64}
+                                            , pointerAddrSpace = AddrSpace 0
+                                            }
+                                            (Name "a")
+                                  , value =
+                                        LocalReference
+                                            IntegerType {typeBits = 64}
+                                            (UnName 1)
+                                  , maybeAtomicity = Nothing
+                                  , alignment = 0
+                                  , metadata = []
+                                  }
+                            , UnName 2 :=
+                              Load
+                              { volatile = False
+                              , address =
+                                    LocalReference
+                                        PointerType
+                                        { pointerReferent =
+                                              IntegerType {typeBits = 64}
+                                        , pointerAddrSpace = AddrSpace 0
+                                        }
+                                        (Name "a")
+                              , maybeAtomicity = Nothing
+                              , alignment = 0
+                              , metadata = []
+                              }
+                            ]
+                            (Do
+                                 Ret
+                                 { returnOperand =
+                                       Just
+                                           (LocalReference
+                                                IntegerType {typeBits = 64}
+                                                (UnName 2))
+                                 , metadata' = []
+                                 })
+                      ]
+                , personalityFunction = Nothing
+                }
+          ]
+    }
