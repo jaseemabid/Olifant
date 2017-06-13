a948d5a5229c1419fb0004fb74a64c8be4da1c4c
Jaseem Abid <jaseemabid@gmail.com>
2017-06-13 18:14:56 -0400

    Start with a super simple frontend calculus

--- src/Olifant/Calculus.hs
+++ src/Olifant/Calculus.hs
@@ -3,3 +3,13 @@ Module      : Olifant.Calculus
 Description : The frontend of the language
 -}
 module Olifant.Calculus where
+
+import Protolude
+
+
+data Calculus =
+    Var Text
+  | Number Int
+  | App Calculus Calculus
+  | Lam Text Calculus
+  | Let Text Calculus
