080aa7cc278b2df0c824293b514120da1e8d3b6a
Jaseem Abid <jaseemabid@gmail.com>
2017-06-16 12:02:24 -0400

    Add failing test cases for wrong local types

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -14,28 +14,45 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "LLVM Code generator" [assign, fn, constfn]
+tests = testGroup "LLVM Code generator" [assign, use, fn, constfn]
 
 assign :: TestTree
-assign = testCase "Global variable assignment" $ do
-  pretty [Let x num] @?= toS genInt
-  pretty [Let x yes] @?= toS genBool
+assign = testCase "Global variables" $
+  pretty [
+      Let (Ref "x" TInt) (Lit (LNumber 42))
+    , Let (Ref "y" TBool) (Lit (LBool True))
+    ] @?= toS gen
 
   where
-    x :: Ref Tipe
-    x = Ref "x" TInt
+    gen :: Text
+    gen = "; ModuleID = 'calc'\n\n\
+          \@x = global i64 42\n\n\
+          \@y = global i1 1"
 
-    num :: Core
-    num = Lit (LNumber 42)
+use :: TestTree
+use = testCase "Use global variable" $
+  pretty progn @?= toS gen
 
-    yes :: Core
-    yes = Lit (LBool True)
+  where
+    -- | Sample program `x -> x`
+    progn :: [Core]
+    progn = [Lam f (Ref "a" TBool) $ Var (Ref "b" TBool)
+           , Lam g (Ref "a" TInt) $ Var (Ref "i" TInt)]
 
-    genInt :: Text
-    genInt = "; ModuleID = 'calc'\n\n@x = global i64 42"
+    f :: Ref Tipe
+    f = Ref "f" (TArrow [TBool, TBool])
 
-    genBool :: Text
-    genBool = "; ModuleID = 'calc'\n\n@x = global i1 1"
+    g :: Ref Tipe
+    g = Ref "g" (TArrow [TInt, TInt])
+
+    gen :: Text
+    gen = "; ModuleID = 'calc'\n\n\
+          \define external ccc i1 @f(ii %a){\n\
+          \entry:\n\
+          \  ret i1 %b\n}\
+          \define external ccc i64 @f(i64 %a){\n\
+          \entry:\n\
+          \  ret i64 %i\n}"
 
 fn :: TestTree
 fn = testCase "Simple identity function" $ pretty [progn] @?= toS gen
@@ -59,6 +76,7 @@ fn = testCase "Simple identity function" $ pretty [progn] @?= toS gen
           \define external ccc i64 @id(i64 %x){\n\
           \entry:\n\
           \  ret i64 %x\n}"
+
 constfn :: TestTree
 constfn = testCase "Const function returning false" $
   pretty [progn] @?= toS gen
