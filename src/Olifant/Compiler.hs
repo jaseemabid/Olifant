e1c985163e740d98b93ed68cb185240965f3e8f0
Jaseem Abid <jaseemabid@gmail.com>
2017-06-16 22:56:14 -0400

    WIP: Add pass to find undefined variables

--- src/Olifant/Compiler.hs
+++ src/Olifant/Compiler.hs
@@ -2,10 +2,10 @@
 Module      : Olifant.Compiler
 Description : Compile Calculus to Core
 -}
-
 module Olifant.Compiler where
 
 import Protolude hiding (cast)
+import qualified Data.Set as Set
 
 import Olifant.Core
 import qualified Olifant.Calculus as C
@@ -23,3 +23,24 @@ cast (C.Bool b) = Lit (LBool b)
 cast (C.App fn arg') = App (cast fn) (cast arg')
 cast (C.Lam n b) = Lam (Ref "~" unit) (Ref n unit) (cast b)
 cast (C.Let var val) = Let (Ref var unit) (cast val)
+
+-- | Find undefined variables
+--
+-- This one can serve as an example for a nano pass!
+--
+undef :: CoreUT -> Either Text CoreUT
+undef core = evalState (undef' core) Set.empty
+  where
+    undef' :: CoreUT -> State (Set Text) (Either Text CoreUT)
+    undef' e@(Var (Ref a _t)) = do
+      x <- Set.member a <$> get
+      return $ if x then Right e else Left a
+
+    undef' (l@Lit{} ) = return $ Right l
+
+    undef' (App _f exp') = undef' exp'
+
+    -- [todo] - Evaluate body with arg as well
+    undef' (Lam _fn arg' body') = undef' (Var arg') >> undef' body'
+
+    undef' (Let _var val) =  undef' val
