813618aa78744984c018e35d8253c4fd30a16acb
Jaseem Abid <jaseemabid@gmail.com>
2017-07-26 21:44:22 -0400

    Write a driver program in C
    Fixes #10

--- runtime/olifant.c
+++ runtime/olifant.c
@@ -1,7 +1,8 @@
 #include "stdio.h"
 
-extern int printi(int X) {
-  printf("%d\n", X);
-  fflush(stdout);
-  return 0;
+int olifant();
+
+int main() {
+   printf("%d\n", olifant());
+   return 0;
 }
