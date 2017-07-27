fe424a319d28df3619ea5bd9e6da72bda3916e6d
Jaseem Abid <jaseemabid@gmail.com>
2017-07-27 16:19:32 -0400

    Change frontend syntax, drop the let keyword
    Ref #5 #6

--- test/Test/Parser.hs
+++ test/Test/Parser.hs
@@ -3,9 +3,7 @@
 -- Known issues
 --
 -- [todo] - Parser doesn't understand precedence with braces
-module Test.Parser
-    ( tests
-    ) where
+module Test.Parser (tests) where
 
 import Protolude hiding (bool)
 
@@ -18,87 +16,53 @@ import Test.Tasty.HUnit
 tests :: TestTree
 tests = testGroup "Parser" ts
   where
-    ts = [literals, lam, combinators, application, let', ll, alias]
+    ts = [literals, lam, application, vars, ll, alias]
 
 literals :: TestTree
-literals =
-    testCase "Literal numbers, identifiers and booleans" $ do
-        expect "42" [CLit $ Number 42]
-        expect "-26" [CLit $ Number (-26)]
-        expect "#f" [CLit $ Bool False]
-        expect "#t" [CLit $ Bool True]
-        expect "hello" [CVar "hello"]
+literals = testCase "Literal numbers, identifiers and booleans" $ do
+    expect "42"    [CLit $ Number 42]
+    expect "-26"   [CLit $ Number (-26)]
+    expect "#f"    [CLit $ Bool False]
+    expect "#t"    [CLit $ Bool True]
+    expect "hello" [CVar "hello"]
+
+vars :: TestTree
+vars = testCase "Variable definitions" $ do
+    expect "a = 1"     [CLet "a" (CLit $ Number 1)]
+    expect "a   =   1" [CLet "a" (CLit $ Number 1)]
+    expect "a=1"       [CLet "a" (CLit $ Number 1)]
 
 lam :: TestTree
-lam =
-    testCase "λ definitions" $
-    -- Identity function
-     do
-        expect "λx.x" [CLam "λ" [(TUnit, "x")] (CVar "x")]
-        expect "λx:b.x" [CLam "λ" [(TBool, "x")] (CVar "x")]
-        expect "λx:i.x" [CLam "λ" [(TInt, "x")] (CVar "x")]
-
-combinators :: TestTree
-combinators =
-    testCase "Combinators" $
-    -- K combinator
-    expect "λx.λy.x" [CLam "λ" [(TUnit, "x")] (CLam "λ" [(TUnit, "y")] (CVar "x"))]
-    -- S combinator
-    -- expect "λx.λy.λz.x z y z" [Lam "x" TUnit
-    --                             (Lam "y" TUnit
-    --                              (Lam "z" TUnit
-    --                                (App (Var "x")
-    --                                 (App (Var "z") (App (Var "y") (Var "z"))))))]
-    --
-    -- B combinator
-    -- expect "λx.λy.λz.x y z" [Lam "x" TUnit
-    --                           (Lam "y" TUnit
-    --                             (Lam "z" TUnit (App (Var "x") (App (Var "y") (Var "z")))))]
+lam = testCase "λ definitions" $ do
+    expect "id x   = x"     [CLam "id" [(TUnit, "x")] (CVar "x")]
+    expect "id x:b = x"     [CLam "id" [(TBool, "x")] (CVar "x")]
+    expect "id x:i = x"     [CLam "id" [(TInt, "x")] (CVar "x")]
+    expect "id x:i y:i = x" [CLam "id" [(TInt, "x"), (TInt, "y")] (CVar "x")]
 
 application :: TestTree
-application =
-    testCase "λ application" $ do
-        expect "a b" d
-    -- Handle some spaces correctly
-        expect " a  b " d
-        expect "a   b" d
+application = testCase "λ application" $ do
+    expect "a b" d
+    expect " a  b " d
+    expect "a   b" d
   where
     d = [CApp (CVar "a") [CVar "b"]]
 
-let' :: TestTree
-let' =
-    testCase "Let expressions" $ do
-        expect "let a = 1" [CLet "a" (CLit $ Number 1)]
-        expect "let a   =   1" [CLet "a" (CLit $ Number 1)]
-        expect "let   a = 1" [CLet "a" (CLit $ Number 1)]
-        expect "let id = /x.x" [CLet "id" (CLam "id" [x] (CVar "x"))]
-  where
-    x = (TUnit, "x")
-
 alias :: TestTree
-alias =
-    testCase "Let bindings with aliases" $
+alias = testCase "Let bindings with aliases" $
     expect "let f = id" [CLet "f" (CVar "id")]
 
 ll :: TestTree
-ll =
-    testCase "Handle sequences of expressions" $ do
-        expect "1; 1" [a, a]
-        expect "1;1" [a, a]
-        expect "1 1" [CApp a [a]]
-        expect "1 2 3" [CApp a [CLit $ Number 2, CLit $ Number 3]]
-        expect "/x.x; 1" [CLam "λ" [x] (CVar "x"), a]
-        expect
-            "let id = /x.x; id 1"
-            [CLet "id" (CLam "id" [x] (CVar "x")), CApp (CVar "id") [a]]
+ll = testCase "Handle sequences of expressions" $ do
+    expect "1; 1"    [a, a]
+    expect "1;1"     [a, a]
+    expect "1 1"     [CApp a [a]]
+    expect "1 2 3"   [CApp a [CLit $ Number 2, CLit $ Number 3]]
+    expect "/x.x; 1" [CLam "λ" [x] (CVar "x"), a]
+    expect "let id = /x.x; id 1"
+      [CLet "id" (CLam "id" [x] (CVar "x")), CApp (CVar "id") [a]]
   where
     a = CLit $ Number 1
     x = (TUnit, "x")
-
--- inline :: TestTree
--- inline =
---     testCase "Support inline operators" $
---     expect "let f = /a:i b:i.a + b; f 1 2" []
 
 -- * Helper functions
 expect :: Text -> [Calculus] -> Assertion
