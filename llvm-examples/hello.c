d5685d1c6d273521b8540f131dbdcc30fd1780b3
Jaseem Abid <jaseemabid@gmail.com>
2018-05-09 14:05:42 +0530

    Fix the sample C code

--- llvm-examples/hello.c
+++ llvm-examples/hello.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int x = 42;
+int x = 12;
 const int y = 2;
 static int c = 3;
 
-int fib(int n) {
+int fact(int n) {
     if (n == 0) {
         return 1;
     } else {
-        return n * fib(n - 1);
+        return n * fact(n - 1);
     }
 }
 
 int main() {
-    printf("hello %d\n", fib(x));
+    printf("hello %d\n", fact(x));
     return 0;
 }
