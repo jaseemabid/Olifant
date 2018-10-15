74a295f6b8c2467804954b4d5f4d5b9e2bae58f8
Jaseem Abid <jaseemabid@monzo.com>
2018-10-15 21:56:00 +0100

    Add some emacs config for intero to work with test files

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode . ((intero-targets . ("Olifant:lib" "Olifant:exe:olifant" "Olifant:test:Olifant-test")))))
